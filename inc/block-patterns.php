7c9405b989d1c555f0061f8b3e668fa64b1815f9
Juan Tonang <rizwanaritonang@gmail.com>
2021-09-15 21:40:01 +0700

    added block-templates and block-template-parts html files

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -10,8 +10,8 @@
 * Register Block Pattern Category.
 **/
 if ( function_exists( 'clay_register_block_pattern_category' ) ) {
-    clay_register_block_pattern_category() {
+    clay_register_block_pattern_category(
         'elements',
         array( 'label' => esc_html__( 'Design elements', 'clay' ) )
-    }
+    );
 }
